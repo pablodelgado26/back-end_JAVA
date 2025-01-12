6df08acd49216a00560df73030d7844c56888d52
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2025-01-12 13:32:55 -0300

    feat: creating entities

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,18 @@
+package com.eventostec.api.domain.address;
+
+import jakarta.persistence.*;
+
+@Table(name = "address")
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -0,0 +1,29 @@
+package com.eventostec.api.domain.coupon;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "event-coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
